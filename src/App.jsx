8f514d5c92782436fef2ccb9c5be8dab4b4037ef
Ryan-MARIN <95910660+Ryan-MARIN@users.noreply.github.com>
2023-02-06 17:10:07 +0100

    CRA nettoyé + FirstComponent

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <FirstComponent />
+        </div>
+    );
+}
+
+export default App;
